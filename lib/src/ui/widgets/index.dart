b3642ade19c218cfac64805504f965a2169f8821
becooq81 <becooq81@gmail.com>
2024-02-23 00:57:19 +0900

    fix: remove unnecessary widget exports

--- lib/src/ui/widgets/index.dart
+++ lib/src/ui/widgets/index.dart
@@ -1,7 +1,5 @@
 // export widget
 export 'features/med_recognizer_widget.dart' show MedRecognizerWidget;
-export 'features/check_rest_widget.dart' show CheckRestWidget;
-export 'features/pour_right_widget.dart' show PourRightWidget;
 export 'components/gesture_slider.dart' show GestureSlider;
 export 'components/home_button_widget.dart' show HomeButtonWidget;
 export 'features/prescribed_med_recognizer_widget.dart' show PrescribedMedRecognizerWidget;
