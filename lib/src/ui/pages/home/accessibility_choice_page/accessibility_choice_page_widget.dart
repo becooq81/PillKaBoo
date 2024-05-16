cd62d920a7be2751c31797b68d9083861455f916
becooq81 <becooq81@gmail.com>
2024-05-16 17:03:37 +0900

    feat: tap upper half or lower half

--- lib/src/ui/pages/home/accessibility_choice_page/accessibility_choice_page_widget.dart
+++ lib/src/ui/pages/home/accessibility_choice_page/accessibility_choice_page_widget.dart
@@ -1,3 +1,5 @@
+import 'package:pillkaboo/src/data/local/shared_preference/app_state.dart';
+
 import '../../../styles/pillkaboo_theme.dart';
 import '../../../../core/pillkaboo_util.dart';
 
@@ -31,8 +33,14 @@ class _AccessibilityChoicePageWidgetState extends State<AccessibilityChoicePageW
   @override
   void dispose() {
     _model.dispose();
-
     super.dispose();
+  }
+
+  void _handleTap(BuildContext context, bool isYes) {
+    final response = isYes ? "예" : "아니오";
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("You selected: $response")),
+    );
   }
 
   @override
@@ -48,326 +56,62 @@ class _AccessibilityChoicePageWidgetState extends State<AccessibilityChoicePageW
 
     context.watch<PKBAppState>();
 
-    double containerHeight = 111.0/892.0 * MediaQuery.of(context).size.height;
-    double containerWidth = 353.0/412.0 * MediaQuery.of(context).size.width;
-    double iconSize = 80.0/892.0 * MediaQuery.of(context).size.height;
-    double textSize = 30.0/892.0 * MediaQuery.of(context).size.height;
+
+    double textSize = 35.0/892.0 * MediaQuery.of(context).size.height;
 
     return GestureDetector(
-      onTap: () => _model.unfocusNode.canRequestFocus
-          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
-          : FocusScope.of(context).unfocus(),
+      onTapUp: (TapUpDetails details) {
+        final screenHeight = MediaQuery.of(context).size.height;
+        final tapPosition = details.globalPosition.dy;
+        if (tapPosition < screenHeight / 2) {
+          _handleTap(context, true); // uses screen reader, hence NO tts
+          PKBAppState().useScreenReader = true;
+        } else {
+          _handleTap(context, false); // doesn't use screen reader, hence TTS
+          PKBAppState().useScreenReader = false;
+        }
+        PKBAppState().isFirstLaunch = false;
+        context.push('/mainMenuPage');
+      },
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: PKBAppState().tertiaryColor,
         body: SafeArea(
           top: true,
-            child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: const AlignmentDirectional(0.0, 0.0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 32.0),
-                  child: Semantics(
-                    label: '약 정보 인식',
-                    container: true,
-                    child: Container(
-                      width: containerWidth,
-                      height: containerHeight,
-                      decoration: BoxDecoration(
-                        color: PKBAppState().tertiaryColor,
-                        borderRadius: BorderRadius.circular(26.0),
-                        border: Border.all(
-                          color: PKBAppState().secondaryColor,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushReplacement('/medRecognitionPage');
-                        },
-                        child: ExcludeSemantics(
-                          excluding: true,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    30.0, 0.0, 0.0, 0.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: SvgPicture.asset(
-                                    'assets/images/main_menu_pill.svg',
-                                    width: iconSize,
-                                    height: iconSize,
-                                    fit: BoxFit.fitHeight,
-                                    colorFilter: ColorFilter.mode(
-                                      PKBAppState().secondaryColor,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
+            child: 
+                Container(
+                  color: Colors.transparent,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            '스크린리더 사용',
+                            style: TextStyle(
+                                fontSize: textSize,
+                                color: PKBAppState().secondaryColor,
+                                decoration: TextDecoration.none,
                               ),
-                        
-                        
-                          Expanded(
-                            child: Center(child: Text(
-                              '약 정보 인식',
-                              style: PillKaBooTheme.of(context)
-                                  .titleMedium
-                                  .override(
-
-                                    fontFamily: PillKaBooTheme.of(context)
-                                        .titleMediumFamily,
-                                    color:
-                                    PKBAppState().secondaryColor,
-                                    fontSize: textSize,
-                                    fontWeight: FontWeight.w900,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey(
-                                        PillKaBooTheme.of(context)
-                                            .titleMediumFamily),
-                                ),
-                              ),),),
-                            ],
-                          ),),
+                            )
+                          ),
+                        ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            '스크린리더 사용 안함',
+                            style: TextStyle(
+                                fontSize: textSize,
+                                color: PKBAppState().secondaryColor,
+                                decoration: TextDecoration.none,
+                              ),
+                            )
+                          ),
                       ),
-                    ),),
+                    ],
+                  ),
                 ),
               ),
-              Align(
-                alignment: const AlignmentDirectional(0.0, 0.0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 32.0),
-                  child: Semantics(
-                    label: '처방약 시간 인식',
-                    container: true,
-                    child: Container(
-                      width: containerWidth,
-                      height: containerHeight,
-                      decoration: BoxDecoration(
-                        color: PKBAppState().tertiaryColor,
-                        borderRadius: BorderRadius.circular(26.0),
-                        border: Border.all(
-                          color: PKBAppState().secondaryColor,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushReplacement('/prescribedMedRecognitionPage');
-                        },
-                        child: ExcludeSemantics(
-                          excluding: true,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    30.0, 0.0, 0.0, 0.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: SvgPicture.asset(
-                                    'assets/images/prescribed.svg',
-                                    width: iconSize,
-                                    height: iconSize,
-                                    fit: BoxFit.fitHeight,
-                                    colorFilter: ColorFilter.mode(
-                                      PKBAppState().secondaryColor,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                  '처방약 시간 인식',
-                                  style: PillKaBooTheme.of(context)
-                                      .titleMedium
-                                      .override(
-                                    fontFamily: PillKaBooTheme.of(context)
-                                        .titleMediumFamily,
-                                    color:
-                                    PKBAppState().secondaryColor,
-                                    fontSize: textSize,
-                                    fontWeight: FontWeight.w900,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey(
-                                        PillKaBooTheme.of(context)
-                                            .titleMediumFamily),
-                                ),
-                              ),),),
-                            ],
-                          ),),
-                      ),
-                    ),),
-                ),
-              ),
-              Align(
-                alignment: const AlignmentDirectional(0.0, 0.0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 32.0),
-                  child: Semantics(
-                    label: '환경 설정',
-                    container: true,
-                    child: Container(
-                      width: containerWidth,
-                      height: containerHeight,
-                      decoration: BoxDecoration(
-                        color: PKBAppState().tertiaryColor,
-                        borderRadius: BorderRadius.circular(26.0),
-                        border: Border.all(
-                          color: PKBAppState().secondaryColor,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushReplacement('/settingsMenuPage');
-                        },
-                        child: ExcludeSemantics(
-                          excluding: true,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    30.0, 0.0, 0.0, 0.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: SvgPicture.asset(
-                                    'assets/images/setting.svg',
-                                    width: iconSize,
-                                    height: iconSize,
-                                    fit: BoxFit.fitHeight,
-                                    colorFilter: ColorFilter.mode(
-                                      PKBAppState().secondaryColor,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                  '환경 설정',
-                                  style: PillKaBooTheme.of(context)
-                                      .titleMedium
-                                      .override(
-                                    fontFamily: PillKaBooTheme.of(context)
-                                        .titleMediumFamily,
-                                    color:
-                                    PKBAppState().secondaryColor,
-                                    fontSize: textSize,
-                                    fontWeight: FontWeight.w900,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey(
-                                        PillKaBooTheme.of(context)
-                                            .titleMediumFamily),
-                                ),
-                              ),),),
-                            ],
-                          ),),
-                      ),
-                    ),),
-                ),
-              ),
-              Align(
-                alignment: const AlignmentDirectional(0.0, 0.0),
-                child: Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 32.0),
-                  child: Semantics(
-                    label: '도움말',
-                    container: true,
-                    child: Container(
-                      width: containerWidth,
-                      height: containerHeight,
-                      decoration: BoxDecoration(
-                        color: PKBAppState().tertiaryColor,
-                        borderRadius: BorderRadius.circular(26.0),
-                        border: Border.all(
-                          color: PKBAppState().secondaryColor,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          context.pushReplacement('/helpPage');
-                        },
-                        child: ExcludeSemantics(
-                          excluding: true,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    40.0, 0.0, 0.0, 0.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: SvgPicture.asset(
-                                    'assets/images/Guide.svg',
-                                    width: iconSize,
-                                    height: iconSize,
-                                    fit: BoxFit.fitHeight,
-                                    colorFilter: ColorFilter.mode(
-                                      PKBAppState().secondaryColor,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                  '도움말',
-                                  style: PillKaBooTheme.of(context)
-                                      .titleMedium
-                                      .override(
-                                    fontFamily: PillKaBooTheme.of(context)
-                                        .titleMediumFamily,
-                                    color:
-                                    PKBAppState().secondaryColor,
-                                    fontSize: textSize,
-                                    fontWeight: FontWeight.w900,
-                                    useGoogleFonts: GoogleFonts.asMap()
-                                        .containsKey(
-                                        PillKaBooTheme.of(context)
-                                            .titleMediumFamily),
-                                ),
-                              ),),),
-                            ],
-                          ),),
-                      ),
-                    ),),
-                ),
-              ),
-            ],
-          ),),
-        ),
-    );
+            ),
+          );
   }
 }
